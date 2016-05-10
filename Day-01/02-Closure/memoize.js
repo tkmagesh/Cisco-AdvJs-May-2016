e953a44ae4a1aae2e729ae95424ee539d2222e93
tkmagesh <tkmagesh77@gmail.com>
2016-05-10 10:06:09 +0530

    memoize

---
+++ Day-01/02-Closure/memoize.js
@@ -0,0 +1,37 @@
+function memoize(fn){
+	var cache = {};
+	return function (n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = fn(n);
+		return cache[n];
+	}
+};
+
+/*create a function that returns true/false depending on the given number is prime or not.  Make sure the algorithm to check doesnt run more than once for the given number.*/
+
+
+var isPrime = memoize(function checkPrime(n){
+		console.log('processing ', n);
+		if (n <= 3) return true;
+		for(var i=2; i<= (n/2); i++)
+			if (n % i === 0) return false;
+		return true;
+	});
+	
+var isEvenOrOdd = memoize(function evenOrOdd(n){
+	console.log('processing ', n);
+	return n % 2 === 0 ? "even" : "odd";
+});
+
+isPrime(100) //=> run the algo
+isPrime(101) //=> run the algo
+isPrime(102) //=> run the algo
+isPrime(103) //=> run the algo
+
+isPrime(100) //=> do NOT run the algo
+isPrime(101) //=> do NOT run the algo
+isPrime(102) //=> do NOT run the algo
+isPrime(103) //=> do NOT run the algo
+
+
+isEvenOrOdd
--- Day-01/02-Closure/spinner.js
+++ Day-01/02-Closure/spinner.js
@@ -30,30 +30,3 @@ function getSpinner(){
 }
 
 
-/*create a function that returns true/false depending on the given number is prime or not.  Make sure the algorithm to check doesnt run more than once for the given number.*/
-
-var isPrime = (function(){
-	var cache = {};
-	function checkPrime(n){
-		console.log('processing ', n);
-		if (n <= 3) return true;
-		for(var i=2; i<= (n/2); i++)
-			if (n % i === 0) return false;
-		return true;
-	}
-	return function (n){
-		if (typeof cache[n] === 'undefined')
-			cache[n] = checkPrime(n);
-		return cache[n];
-	}
-})();
-
-isPrime(100) //=> run the algo
-isPrime(101) //=> run the algo
-isPrime(102) //=> run the algo
-isPrime(103) //=> run the algo
-
-isPrime(100) //=> do NOT run the algo
-isPrime(101) //=> do NOT run the algo
-isPrime(102) //=> do NOT run the algo
-isPrime(103) //=> do NOT run the algo
